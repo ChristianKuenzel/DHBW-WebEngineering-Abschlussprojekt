36a7c57aaa1032991b36e898a8d288213fa843a5
David Schüler <David.schueler97@gmail.com>
2020-12-10 20:41:32 +0100

    index.html, script.js

--- client/script.js
+++ client/script.js
@@ -1,18 +1,7 @@
-/*
 
-Copyright 2020
-DHBW Lörrach, WebEngineering Abschlussprojekt: Server-Client based WebChatApplication
-David Schüler, Matr.Nr.: ?, <david.schueler97@gmail.com>, https://github.com/AranguZ
-Christian Künzel, Matr.Nr.: 3889521, <kunibertgames@web.de>, https://github.com/ChristianKuenzel
+// Haeder
 
-Application got created based on IP & work of Mathis Zeiher <GitHub: mzeiher>
 
-Content undergoes the terms of chosen licenses. See GitHub for more:
-https://github.com/ChristianKuenzel/DHBW-WebEngineering-Abschlussprojekt
-
-HTML Skript
-
-*/
 // __________________________________________________________________________________________
 // safe references to DOM nodes in variables for easy access
 const messageInput = document.querySelector('#message');
@@ -21,13 +10,16 @@ const messageArea = document.querySelector('#output');
 
 // __________________________________________________________________________________________
 // Execute main function init()
+
+// Create client object.
+let client = {};
 init();
 
 // __________________________________________________________________________________________
 // Define main function init()
 async function init() {
-    // Create client object.
-    let client = {};
+    
+    
 
     // Get the user's location and add it to client object.
     const host = window.location.host;
@@ -41,11 +33,15 @@ async function init() {
     const userName = getUserName();
     client.userName = userName;
 
+
+
     // Request all messages that had already been written from the server.
     // Convert into JSON and add them to the message area.
     const messagesResult = await fetch('/messages', {method: 'GET'});
     const messages = await messagesResult.json();
     messages.forEach(addMessage);
+
+
 
     // create a new websocket connection
     const websocket = new WebSocket(`ws://${host}/ws`);
@@ -54,6 +50,8 @@ async function init() {
     websocket.addEventListener('message', (messageEvent) => {
         addMessage(messageEvent.data);
     });
+
+
 
     // add a click listener to the button to send a new message
     messageButton.addEventListener('click', () => {
@@ -77,6 +75,8 @@ async function init() {
         messageInput.value = '';
     });
 
+
+
     // add an event listener to the text-area to be able to send messages with a shift+enter
     messageInput.addEventListener('keydown', (evt) => {
         // if shift or ctrl + enter is typed send a new message
@@ -97,18 +97,64 @@ async function init() {
 // __________________________________________________________________________________________
 // add a message to the message area
 function addMessage(jsonString) {
+    /*
+<div class="messagecontainer">
+    <div class="author">
+            user
+        </div>
+        <div>
+            hello how are you today
+        <span class="time">11:00</span>
+    </div>
+ </div>
+ let content = {
+            "clientID": clientId,
+            "userName": userName,
+            "message": messageInput.value,
+            "type": "normal",
+            "time": timeStamp
+        }
+    */
     let content = JSON.parse(jsonString);
 
-    const pElement = document.createElement('p');
+    const messagecontainer = document.createElement('div');
+
+    const author = document.createElement('div');
+
+    const textarea = document.createElement('div');
+
+    const time = document.createElement('span');
+
+     // add username    
+    author.textContent = content.userName;
+
+    author.className = "author";
+
+    messagecontainer.className = "messagecontainer";
+
+    if (client.clientId === content.clientID) {
+        messagecontainer.className = "messagecontainer me";
+    
+    }
 
     // Add text of message object.
-    pElement.innerHTML = content.message;
+    textarea.innerHTML = content.message;
+
+    time.textContent = content.time;
+
+    time.className = "time";
+
+    textarea.appendChild(time);
+
+    messagecontainer.appendChild(author);
+
+    messagecontainer.appendChild(textarea);
 
     // Append the p element to the message area
-    messageArea.appendChild(pElement);
+    messageArea.appendChild(messagecontainer);
 
     // Scroll the element into view
-    pElement.scrollIntoView();
+    messagecontainer.scrollIntoView();
 }
 
 // Create a random id based on present time. Due to unique date-time values and the measurement of
